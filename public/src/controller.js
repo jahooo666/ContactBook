9bae9f68a1f1c3ae6ddfa2aecb7395d3ea78cce7
jahooo666 <okraskaj@ee.pw.edu.pl>
2016-03-21 21:14:22 +0100

    Working list view, as well as redirection to a single contact view. Fixed bugs from before.

--- public/src/controller.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Jasiek on 2016-03-20.
- */
-angular.module('ContactsApp')
-    .controller('ListController', function ($scope){
-        $scope.contacts = [];
-    });
---
+++ public/src/controllers.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Jasiek on 2016-03-20.
+ */
+angular.module('ContactsApp')
+    .controller('ListController', function ($scope,Contact, $location){
+        $scope.contacts = Contact.query();
+        $scope.fields = ['firstName','lastName'];
+
+        $scope.sort = function (field){
+            $scope.sort.field = field;
+            $scope.sort.order = !$scope.sort.order;
+        };
+
+        $scope.sort.field = 'firstName';
+        $scope.sort.order = false;
+
+        $scope.show = function (id){
+            $location.url('/contact/' + id);
+        };
+    });
