b91090a3608efb44401361cf84fd3f307ba910b3
jahooo666 <okraskaj@ee.pw.edu.pl>
2016-03-27 20:19:25 +0200

    >new field directive complete! >adding new fileds works fine and also saves and  removes the field if you want

--- public/src/filters.js
+++ public/src/filters.js
@@ -10,6 +10,15 @@ angular.module('ContactsApp')
         };
     })
 
+    .filter('camelCase', function(){
+       // First Name -> firstName
+        return function (input) {
+            return input.toLowerCase().replace(/ (\w)/g, function(match, letter){
+               return letter.toUpperCase();
+            });
+        };
+    })
+
     .filter('keyFilter', function () {
             return function(obj,query){
             var result = {};
